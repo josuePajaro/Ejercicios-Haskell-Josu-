ec23135463df2ddc60b40b8556ecc95aabcdc06d
josuePajaro <j0zu3.pajaro@gmail.com>
2025-07-02 18:22:12 -0600

    Add files via upload

---
+++ Ejercicio5.7.1.hs
@@ -0,0 +1,3 @@
+-- usando una comprensión de lista, da una expresión que calcule la suma 1^2 + 2^2 + ... + 100^2 de los primeros 100 cuadrados enteros.
+
+--sum [ x^2 | x <- [1..100]] escrbiendo esta expresion en ghci obtenemos la suma requerida.
